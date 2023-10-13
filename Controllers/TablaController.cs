dab83670dd733fff40124b0dd95f2ea99978f2dd
Gabriel Fernando <gabrielsxf@gmail.com>
2023-10-13 18:52:17 -0500

    first commit

---
+++ Controllers/TablaController.cs
@@ -0,0 +1,128 @@
+﻿using MVCCrud.Models;
+using MVCCrud.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using(CrudEntities1 db = new CrudEntities1())
+            {
+                lst = (from d in db.tablo
+                           select new ListTablaViewModel
+                           {
+                               Id = d.id,
+                               Nombre= d.nombre,
+                               Correo= d.correo,
+                           }).ToList();
+
+            }
+
+            return View(lst);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    using(CrudEntities1 db = new CrudEntities1())
+                    {
+                        var oTabla = new tablo();
+
+                        oTabla.correo = model.Correo;
+                        oTabla.nombre = model.Nombre;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+
+                        db.tablo.Add(oTabla);
+                        db.SaveChanges();
+
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using(CrudEntities1 db = new CrudEntities1())
+            {
+                var oTabla = db.tablo.Find(id);
+                model.Id = oTabla.id;
+                model.Nombre= oTabla.nombre;
+                model.Correo= oTabla.correo;
+                //model.Fecha_Nacimiento = (DateTime)oTabla.fecha_nacimiento;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (CrudEntities1 db = new CrudEntities1())
+                    {
+                        var oTabla = db.tablo.Find(model.Id);
+
+                        oTabla.correo = model.Correo;
+                        oTabla.nombre = model.Nombre;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            using(CrudEntities1 db = new CrudEntities1())
+            {
+                var Otabla = db.tablo.Find(id);
+                db.tablo.Remove(Otabla);
+                db.SaveChanges();
+            }
+
+            return Redirect("~/Tabla/Index");
+        }
+    }
+}
---
+++ Models/ViewModels/TablaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrud.Models.ViewModels
+{
+    public class TablaViewModel
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        [Display(Name = "Correo")]
+        public string Correo { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha_Nacimiento { get; set; }
+    }
+}
